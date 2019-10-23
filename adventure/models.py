9954ff87e4f7332a8fbe93be96afa08022b12873
dustiny5 <dustiny5@users.noreply.github.com>
2019-10-23 10:52:02 -0700

    Added __str__ function

--- adventure/models.py
+++ adventure/models.py
@@ -38,6 +38,8 @@ class Room(models.Model):
         return [p.user.username for p in Player.objects.filter(currentRoom=self.id) if p.id != int(currentPlayerID)]
     def playerUUIDs(self, currentPlayerID):
         return [p.uuid for p in Player.objects.filter(currentRoom=self.id) if p.id != int(currentPlayerID)]
+    def __str__(self):
+        return self.title
 
 
 class Player(models.Model):
@@ -56,6 +58,8 @@ class Player(models.Model):
         except Room.DoesNotExist:
             self.initialize()
             return self.room()
+    def __str__(self):
+        return self.user
 
 @receiver(post_save, sender=User)
 def create_user_player(sender, instance, created, **kwargs):
